98d47ab7a09bf6dfac4d66b3b1731fce1bd577d3
mtanneau <mathieu.tanneau@gmail.com>
2025-05-12 12:23:13 -0400

    Add return type annotation for OnStatus

--- src/RepoAuditor/ExecuteModules.py
+++ src/RepoAuditor/ExecuteModules.py
@@ -140,7 +140,7 @@ def Execute(
                     status: ExecuteTasks.Status,
                 ) -> ExecuteTasks.TransformResultComplete:
                     # ----------------------------------------------------------------------
-                    def OnStatus(num_completed: int, *args, **kwargs):
+                    def OnStatus(num_completed: int, *args, **kwargs) -> bool:
                         status.OnProgress(
                             num_completed,
                             _CreateStatusString(*args, **kwargs),
