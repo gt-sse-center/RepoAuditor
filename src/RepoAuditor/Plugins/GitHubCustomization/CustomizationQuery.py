e286e6189a032371b69904fe6a8f1301fba791b8
Varun Agrawal <varagrawal@gmail.com>
2025-05-30 15:29:11 -0400

    GitHubCustomization plugin part 3
    GitHubCustomization Functionality and Unit Tests

--- src/RepoAuditor/Plugins/GitHubCustomization/CustomizationQuery.py
+++ src/RepoAuditor/Plugins/GitHubCustomization/CustomizationQuery.py
@@ -6,7 +6,13 @@
 # -------------------------------------------------------------------------------
 """Contains the CustomizationQuery object."""
 
+from typing import Any, Optional
+
+from dbrownell_Common.Types import override  # type: ignore[import-untyped]
+
 from RepoAuditor.Query import ExecutionStyle, Query
+from RepoAuditor.Plugins.GitHubCustomization.Requirements.CodeOwners import CodeOwners
+from RepoAuditor.Plugins.GitHubCustomization.Requirements.Contributing import Contributing
 
 
 class CustomizationQuery(Query):
@@ -16,5 +22,24 @@ class CustomizationQuery(Query):
         super().__init__(
             "CustomizationQuery",
             ExecutionStyle.Parallel,
-            [],  # Empty requirements list for now
+            [
+                CodeOwners(),
+                Contributing(),
+            ],
         )
+
+    # ----------------------------------------------------------------------
+    @override
+    def GetData(
+        self,
+        module_data: dict[str, Any],
+    ) -> Optional[dict[str, Any]]:
+        """Get the data from an API session."""
+        response = module_data["session"].get("")
+
+        response.raise_for_status()
+        response = response.json()
+
+        module_data["standard"] = response
+
+        return module_data
