4063453b2acc60b9a45b701c31a69e464e5e2d82
Varun Agrawal <varagrawal@gmail.com>
2025-06-23 15:34:11 -0400

    Update CommunityStandards tests to remove need for custom branch

--- tests/Plugins/GitHubCommunityStandards_EndToEndTest.py
+++ tests/Plugins/GitHubCommunityStandards_EndToEndTest.py
@@ -6,19 +6,50 @@
 # -------------------------------------------------------------------------------
 """End-to-end tests for the GitHubCommunityStandards plugin."""
 
+import shutil
 from pathlib import Path
+from tempfile import TemporaryDirectory
+from unittest.mock import patch
 
 import pytest
 from dbrownell_Common.TestHelpers.StreamTestHelpers import (
     InitializeStreamCapabilities,
 )
+from git import Repo
 from typer.testing import CliRunner
 from utilities import CheckPATFileExists, GetGithubUrl, ScrubDurationGithuburlAndSpaces
 
 from RepoAuditor.EntryPoint import app
+from RepoAuditor.Plugins.GitHubCommunityStandards.CommunityStandardsQuery import CommunityStandardsQuery
 
 # ----------------------------------------------------------------------
 pytest.fixture(InitializeStreamCapabilities(), scope="session", autouse=True)
+
+
+def GetDataFunctional(path_to_delete: str, is_directory=False):
+    """A functional which returns a modified GetData method which
+    deletes the specified path in the cloned repo.
+    """
+
+    def GetData(self, module_data):
+        # Clone the GitHub repository to a temp directory
+        github_url = module_data["url"]
+        branch = module_data.get("branch", "main")
+        temp_repo_dir = TemporaryDirectory()
+        Repo.clone_from(github_url, temp_repo_dir.name, branch=branch)
+
+        path = Path(temp_repo_dir.name) / path_to_delete
+
+        if is_directory:
+            shutil.rmtree(path)
+        else:
+            path.unlink()
+
+        # Record the path and the temp directory for later cleanup
+        module_data["repo_dir"] = temp_repo_dir
+        return module_data
+
+    return GetData
 
 
 # ----------------------------------------------------------------------
@@ -28,21 +59,24 @@ class TestCommunityStandards:
     # ----------------------------------------------------------------------
     def test_CodeOwners(self, pat_args, snapshot):
         """Test if a CODEOWNERS file exists in the repository."""
-        result = CliRunner().invoke(app, pat_args + ["--GitHubCommunityStandards-CodeOwners-exists"])
+        result = CliRunner().invoke(
+            app,
+            pat_args + ["--GitHubCommunityStandards-CodeOwners-exists"],
+        )
 
         assert result.exit_code == 0, result.output
         assert ScrubDurationGithuburlAndSpaces(result.stdout) == snapshot
 
+    @patch.object(
+        CommunityStandardsQuery,
+        "GetData",
+        GetDataFunctional(".github/CODEOWNERS"),
+    )
     def test_NoCodeOwners(self, pat_args, snapshot):
         """Test if a CODEOWNERS file does not exist in the repository."""
         result = CliRunner().invoke(
             app,
-            pat_args
-            + [
-                "--GitHubCommunityStandards-CodeOwners-exists",
-                "--GitHubCommunityStandards-branch",
-                "test-GitHubCommunityStandards",
-            ],
+            pat_args + ["--GitHubCommunityStandards-CodeOwners-exists"],
         )
 
         assert result.exit_code == -1, result.output
@@ -50,21 +84,24 @@ class TestCommunityStandards:
 
     def test_Contributing(self, pat_args, snapshot):
         """Test if a CONTRIBUTING.md file exists in the repository."""
-        result = CliRunner().invoke(app, pat_args + ["--GitHubCommunityStandards-Contributing-exists"])
+        result = CliRunner().invoke(
+            app,
+            pat_args + ["--GitHubCommunityStandards-Contributing-exists"],
+        )
 
         assert result.exit_code == 0, result.output
         assert ScrubDurationGithuburlAndSpaces(result.stdout) == snapshot
 
+    @patch.object(
+        CommunityStandardsQuery,
+        "GetData",
+        GetDataFunctional("CONTRIBUTING.md"),
+    )
     def test_NoContributing(self, pat_args, snapshot):
         """Test if a CONTRIBUTING file does not exist in the repository."""
         result = CliRunner().invoke(
             app,
-            pat_args
-            + [
-                "--GitHubCommunityStandards-Contributing-exists",
-                "--GitHubCommunityStandards-branch",
-                "test-GitHubCommunityStandards",
-            ],
+            pat_args + ["--GitHubCommunityStandards-Contributing-exists"],
         )
 
         assert result.exit_code == -1, result.output
@@ -77,38 +114,44 @@ class TestCommunityStandards:
         assert result.exit_code == 0, result.output
         assert ScrubDurationGithuburlAndSpaces(result.stdout) == snapshot
 
+    @patch.object(
+        CommunityStandardsQuery,
+        "GetData",
+        GetDataFunctional(".github/ISSUE_TEMPLATE", True),
+    )
     def test_NoIssueTemplate(self, pat_args, snapshot):
         """Test if no Issue template file exists in the repository."""
         result = CliRunner().invoke(
             app,
-            pat_args
-            + [
-                "--GitHubCommunityStandards-IssueTemplates-exists",
-                "--GitHubCommunityStandards-branch",
-                "test-GitHubCommunityStandards",
-            ],
+            pat_args + ["--GitHubCommunityStandards-IssueTemplates-exists"],
         )
 
         assert result.exit_code == -1, result.output
         assert ScrubDurationGithuburlAndSpaces(result.stdout) == snapshot
 
     def test_PullRequestTemplate(self, pat_args, snapshot):
-        """Test if a Pull Request template file (e.g. PULL_REQUEST_TEMPLATE.md) exists in the repository."""
-        result = CliRunner().invoke(app, pat_args + ["--GitHubCommunityStandards-PullRequestTemplate-exists"])
+        """
+        Test if a Pull Request template file (e.g. PULL_REQUEST_TEMPLATE.md)
+        exists in the repository.
+        """
+        result = CliRunner().invoke(
+            app,
+            pat_args + ["--GitHubCommunityStandards-PullRequestTemplate-exists"],
+        )
 
         assert result.exit_code == 0, result.output
         assert ScrubDurationGithuburlAndSpaces(result.stdout) == snapshot
 
+    @patch.object(
+        CommunityStandardsQuery,
+        "GetData",
+        GetDataFunctional(".github/pull_request_template.md"),
+    )
     def test_NoPullRequestTemplate(self, pat_args, snapshot):
         """Test if no Pull Request template file exists in the repository."""
         result = CliRunner().invoke(
             app,
-            pat_args
-            + [
-                "--GitHubCommunityStandards-PullRequestTemplate-exists",
-                "--GitHubCommunityStandards-branch",
-                "test-GitHubCommunityStandards",
-            ],
+            pat_args + ["--GitHubCommunityStandards-PullRequestTemplate-exists"],
         )
 
         assert result.exit_code == -1, result.output
@@ -116,21 +159,24 @@ class TestCommunityStandards:
 
     def test_SecurityPolicy(self, pat_args, snapshot):
         """Test if a Security policy file (e.g. SECURITY.md) exists in the repository."""
-        result = CliRunner().invoke(app, pat_args + ["--GitHubCommunityStandards-SecurityPolicy-exists"])
+        result = CliRunner().invoke(
+            app,
+            pat_args + ["--GitHubCommunityStandards-SecurityPolicy-exists"],
+        )
 
         assert result.exit_code == 0, result.output
         assert ScrubDurationGithuburlAndSpaces(result.stdout) == snapshot
 
+    @patch.object(
+        CommunityStandardsQuery,
+        "GetData",
+        GetDataFunctional("SECURITY.md"),
+    )
     def test_NoSecurityPolicy(self, pat_args, snapshot):
         """Test if no Security policy file exists in the repository."""
         result = CliRunner().invoke(
             app,
-            pat_args
-            + [
-                "--GitHubCommunityStandards-SecurityPolicy-exists",
-                "--GitHubCommunityStandards-branch",
-                "test-GitHubCommunityStandards",
-            ],
+            pat_args + ["--GitHubCommunityStandards-SecurityPolicy-exists"],
         )
 
         assert result.exit_code == -1, result.output
